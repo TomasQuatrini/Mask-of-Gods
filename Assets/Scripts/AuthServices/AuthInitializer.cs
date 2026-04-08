5eb8cd01351b2e5f6a83c451497e64e7450239f5
Tomas Quatrini <tomas_quatrini11@hotmail.com>
2026-04-08 04:07:13 -0300

    add UI Message, with Auth errros & message linkeds

--- Assets/Scripts/AuthServices/AuthInitializer.cs
+++ Assets/Scripts/AuthServices/AuthInitializer.cs
@@ -4,16 +4,37 @@ using Unity.Services.Authentication;
 using UnityEngine;
 using UnityEngine.UI;
 using System.Threading.Tasks;
+using TMPro;
 public class AuthInitializer : MonoBehaviour
 {
-
+    [Header("Buttons")]
     [SerializeField] private Button signInAnonymousButton;
+    [SerializeField] private Button signUpAccountButton;
+    [SerializeField] private Button signInWithUsernameAndPasswordButton;
+
+    [Header("Input Fields")]
+    [SerializeField] private TMP_InputField usernameInputField;
+    [SerializeField] private TMP_InputField passwordInputField;
+
+    [Header("Message Popup")]
+    [SerializeField] private UI_Message uiMessage;
+
     async void Start()
     {
         signInAnonymousButton.interactable = false;
         await UnityServices.InitializeAsync();
         signInAnonymousButton.interactable = true;
         signInAnonymousButton.onClick.AddListener(SignIn);
+
+        signUpAccountButton.onClick.AddListener(() =>
+        {
+            SignUpWithUsernamePasswordAsync(usernameInputField.text, passwordInputField.text);
+        });
+
+        signInWithUsernameAndPasswordButton.onClick.AddListener(() =>
+        {
+            SignInWithUsernamePasswordAsync(usernameInputField.text, passwordInputField.text);
+        });
     }
     private async void SignIn()
     {
@@ -24,14 +45,65 @@ public class AuthInitializer : MonoBehaviour
         }
         catch (Exception e)
         {
+            uiMessage.ShowErrorMessage(e.Message);
             Debug.LogError(e);
-            throw;
+            return;
         }
 
         Debug.Log("Player ID: " + AuthenticationService.Instance.PlayerId);
         await Task.Delay(4000);
+        uiMessage.ShowMessage("SignIn Anonymous is successful");
         Debug.Log("Sign in successful");
     }
 
+    async Task SignUpWithUsernamePasswordAsync(string username, string password)
+    {
+        try
+        {
+            await AuthenticationService.Instance.SignUpWithUsernamePasswordAsync(username, password);
+        }
+        catch (AuthenticationException ex)
+        {
+            // Compare error code to AuthenticationErrorCodes
+            // Notify the player with the proper error message            
+            uiMessage.ShowErrorMessage(ex.Message);
+            Debug.LogException(ex);
+            throw;
+        }
+        catch (RequestFailedException ex)
+        {
+            // Compare error code to CommonErrorCodes
+            // Notify the player with the proper error message
+            uiMessage.ShowErrorMessage(ex.Message);
+            Debug.LogException(ex);
+            throw;
+        }
+        uiMessage.ShowMessage("SignUp is successful.");
+    }
 
+    async Task SignInWithUsernamePasswordAsync(string username, string password)
+    {
+        try
+        {
+            await AuthenticationService.Instance.SignInWithUsernamePasswordAsync(username, password);
+
+        }
+        catch (AuthenticationException ex)
+        {
+            // Compare error code to AuthenticationErrorCodes
+            // Notify the player with the proper error message
+            uiMessage.ShowErrorMessage(ex.Message);
+            Debug.LogException(ex);
+            throw;
+        }
+        catch (RequestFailedException ex)
+        {
+            // Compare error code to CommonErrorCodes
+            // Notify the player with the proper error message
+            uiMessage.ShowErrorMessage(ex.Message);
+            Debug.LogException(ex);
+            throw;
+        }
+        uiMessage.ShowMessage("SignIn is successful.");
+    }
 }
---
+++ Assets/Scripts/UI/Text/UI_Message.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class UI_Message : MonoBehaviour
+{
+    [SerializeField] private TMP_Text messageText;
+    private Button closeWindowButton;
+    [SerializeField] private GameObject root;
+
+    
+    void Awake()
+    {             
+        if (root == null) 
+        {
+            Debug.LogError("Root GameObject is not assigned in the inspector.");
+            return;
+        }
+        closeWindowButton = GetComponentInChildren<Button>();
+        closeWindowButton.onClick.AddListener(CloseWindow);
+    }
+
+    private void Start()
+    {
+        CloseWindow();
+    }
+
+    private void CloseWindow()
+    {
+        root.SetActive(false);
+    }
+
+    public void ShowMessage(string message)
+    {        
+        messageText.color = Color.black;
+        messageText.text = message;
+        root.SetActive(true);
+    }
+
+    public void ShowErrorMessage(string message)
+    {
+        messageText.color = Color.red;
+        messageText.text = message;
+        root.SetActive(true);
+    }
+}
